facd0215e13179aeda3fc2b13ec93038000908d4
Faruq Afolabi <gbolayo13@gmail.com>
2022-01-24 16:51:24 -0500

    Initial commit

---
+++ EECS3311-Lab1/eecs3311-lab1/src/york/eecs/source/VertexExistsException.java
@@ -0,0 +1,14 @@
+package york.eecs.source;
+
+/**
+ * This is the exception class
+ * @author wangs
+ *
+ */
+public class VertexExistsException extends Exception{
+	
+	public VertexExistsException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ EECS3311-Lab1/eecs3311-lab1/src/york/eecs/test/StudentTest.java
@@ -0,0 +1,16 @@
+package york.eecs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	 /**
+	  * TODO: Please write at least 5 test cases for testing @UndirectedGraph.
+	  * TODO: Please write at least 5 test cases for testing @UndirectedGraphAlgorithms.
+	  */
+
+}
